664c32bcdccac8dc9863019b26541f83068af3d6
HanNabu <uniondirfolder@gmail.com>
2020-08-16 19:48:35 +0300

    TrieNode

--- DataStructures/TrieNode.cpp
+++ DataStructures/TrieNode.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 class TrieNode
 {
-	map<char, TrieNode*> table;
-	bool isWord;
-
+	
 public:
 	TrieNode() { isWord = false; }
 
-	bool haveRecord(char &c) 
+	bool haveRecord(char c) 
 	{
-		if (!table.empty())
+		if (!haveNoRecord())
 		{
 			auto search = table.find(c);
 			if (search != table.end())
@@ -24,6 +22,57 @@ public:
 		else
 			return false;
 	}
+
+	bool haveNoRecord() 
+	{ return table.empty(); }
+
+	TrieNode* followChar(char c) 
+	{
+		TrieNode* node = nullptr;
+
+		if (!haveNoRecord())
+		{
+			auto search = table.find(c);
+			if (search != table.end())
+			{
+				node = search->second;
+			}
+		}
+
+		return node;
+	}
+
+	void addRecord(char c, TrieNode* n) 
+	{
+		if (!haveRecord(c)) 
+		{
+			table.insert(make_pair(c, n));
+		}
+	}
+
+	void deleteRecord(char c) 
+	{
+		if (haveRecord(c)) 
+		{
+			auto find = table.find(c);
+			if (find != table.end())
+				table.erase(find);
+		}
+	}
+
+	bool getIsWord()
+	{ 
+		return isWord;
+	}
+
+	void setIsWord() 
+	{
+		isWord = true;
+	}
+
+private:
+	map<char, TrieNode*> table;
+	bool isWord;
 };
 
 
